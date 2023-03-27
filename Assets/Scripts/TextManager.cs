207b82efb269092cf6fd4b2d7b460eb56d41dafc
Berk KILIÇ <berk.kilic0638@gmail.com>
2023-03-27 20:34:21 +0300

    Level 3 add - lego add gravity- Level Text Edited

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TextManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText; // Text nesnenizin referansý
     public TextMeshProUGUI panelScoreText; // Text nesnenizin referansý
+    public TextMeshProUGUI levelText; // Text nesnenizin referansý
 
 
 
@@ -16,12 +18,28 @@ public class TextManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (SceneManager.GetActiveScene().buildIndex)
+        {
+            case 0:
+                levelText.text = "LEVEL 1";
+                break;
+            case 1:
+                levelText.text = "LEVEL 2";
+                break;
+            case 2:
+                levelText.text = "LEVEL 3";
+                break;
+            default:
+                levelText.text = "LEVEL " + (SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+
         scoreText.text = "$ " + score.ToString();
         //panelScoreText.text = scoreText.text;
         panelScoreText.text = "$ " + score.ToString();
